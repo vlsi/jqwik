2148bb7afebb9a40a8762f7439b857a9b9008c3d
Vladimir Sitnikov <sitnikov@netcracker.com>
2022-05-28 20:32:22 +0300

    generalize GenericEdgeCasesProperties#consistentItemOrder

--- engine/src/test/java/net/jqwik/api/edgeCases/GenericEdgeCasesProperties.java
+++ engine/src/test/java/net/jqwik/api/edgeCases/GenericEdgeCasesProperties.java
@@ -6,6 +6,7 @@ import net.jqwik.engine.*;
 import net.jqwik.engine.support.*;
 
 import java.util.*;
+import java.util.function.*;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -25,50 +26,99 @@ public interface GenericEdgeCasesProperties {
 	}
 
 	@Property
-	default void mapEachProducesConsistentItemOrder(@ForAll("arbitraries") Arbitrary<?> arbitrary, @ForAll Random random, @ForAll @IntRange(min = 1, max = 20) int size) {
-		Arbitrary<Set<Object>> values = arbitrary.<Object>map(x -> new Object() {
-			@Override
-			public String toString() {
-				return JqwikStringSupport.displayString(x);
-			}
-		}).set().mapEach((set, value) -> value);
-		RandomGenerator<Set<Object>> gen = values.generator(1000, true);
+	default void consistentItemOrder(
+		@ForAll("arbitraries") Arbitrary<?> arbitrary,
+		@ForAll("arbitraryTransformations") ArbitraryTransformer<?, ?> transformation,
+		@ForAll Random random,
+		@ForAll @IntRange(min = 1, max = 20) int size,
+		@ForAll @IntRange(min = 1, max = 1000) int genSize,
+		@ForAll boolean withEdgeCases
+	) {
+		@SuppressWarnings({"unchecked", "rawtypes"})
+		Arbitrary<?> values = transformation.apply((Arbitrary) arbitrary);
+		RandomGenerator<?> gen = values.generator(genSize, withEdgeCases);
 		for (int i = 0; i < size; i++) {
 			long seed = random.nextLong();
-			assertThat(JqwikStringSupport.displayString(gen.next(SourceOfRandomness.newRandom(seed)).value()))
-				.isEqualTo(JqwikStringSupport.displayString(gen.next(SourceOfRandomness.newRandom(seed)).value()));
+			// Both values should be equal with respect to "displayString"
+			// For instance, element order in the sets should match
+			Object valueA = gen.next(SourceOfRandomness.newRandom(seed)).value();
+			Object valueB = gen.next(SourceOfRandomness.newRandom(seed)).value();
+			assertThat(JqwikStringSupport.displayString(valueA))
+				.isEqualTo(JqwikStringSupport.displayString(valueB));
 		}
 	}
 
-	@Property
-	default void flatMapEachProducesConsistentItemOrder(@ForAll("arbitraries") Arbitrary<?> arbitrary, @ForAll Random random, @ForAll @IntRange(min = 1, max = 20) int size) {
-		Arbitrary<Set<Object>> values = arbitrary.<Object>map(x -> new Object() {
-			@Override
-			public String toString() {
-				return JqwikStringSupport.displayString(x);
-			}
-		}).set().flatMapEach((set, value) -> Arbitraries.just(value));
-		RandomGenerator<Set<Object>> gen = values.generator(1000, true);
-		for (int i = 0; i < size; i++) {
-			long seed = random.nextLong();
-			assertThat(JqwikStringSupport.displayString(gen.next(SourceOfRandomness.newRandom(seed)).value()))
-				.isEqualTo(JqwikStringSupport.displayString(gen.next(SourceOfRandomness.newRandom(seed)).value()));
+	interface ArbitraryTransformer<U, V> extends Function<Arbitrary<U>, Arbitrary<V>> {
+		static <E, R> ArbitraryTransformer<E, R> transformer(
+			String name,
+			Function<? super Arbitrary<E>, ? extends Arbitrary<R>> action
+		) {
+			return new ArbitraryTransformer<E, R>() {
+				@Override
+				public String toString() {
+					return name;
+				}
+
+				@Override
+				public Arbitrary<R> apply(Arbitrary<E> input) {
+					return action.apply(input);
+				}
+			};
 		}
+
+		default <K> ArbitraryTransformer<U, K> then(ArbitraryTransformer<V, K> after) {
+			return transformer(
+				toString() + after,
+				(x) -> after.apply(apply(x))
+			);
+		}
+
+		static Object wrapWithOpaqueObject(Object that) {
+			return new Object() {
+				@Override
+				public String toString() {
+					return JqwikStringSupport.displayString(that);
+				}
+			};
+		}
+
+		ArbitraryTransformer<Object, Object> IDENTITY = ArbitraryTransformer.transformer(
+			"identity",
+			(x) -> x
+		);
+
+		ArbitraryTransformer<Object, Object> WRAP_WITH_OPAQUE_OBJECT = ArbitraryTransformer.transformer(
+			".map(::wrapWithOpaqueObject)",
+			(x) -> x.map(ArbitraryTransformer::wrapWithOpaqueObject)
+		);
+
+		ArbitraryTransformer<Object, Set<Object>> SET = ArbitraryTransformer.transformer(
+			".set()",
+			(x) -> x.set()
+		);
+
+		ArbitraryTransformer<Object, Set<Object>> SET__MAP_EACH = ArbitraryTransformer.transformer(
+			".mapEach(::identity)",
+			(x) -> x.set().mapEach((set, value) -> value)
+		);
+
+		ArbitraryTransformer<Object, Set<Object>> SET__FLAT_MAP_EACH = ArbitraryTransformer.transformer(
+			".set().flatMapEach(just(value))",
+			(x) -> x.set().flatMapEach((set, value) -> Arbitraries.just(value))
+		);
 	}
 
-	@Property
-	default void uniqueElementsProducesConsistentItemOrder(@ForAll("arbitraries") Arbitrary<?> arbitrary, @ForAll Random random, @ForAll @IntRange(min = 1, max = 20) int size) {
-		Arbitrary<Set<Object>> values = arbitrary.<Object>map(x -> new Object() {
-			@Override
-			public String toString() {
-				return JqwikStringSupport.displayString(x);
-			}
-		}).set().uniqueElements(JqwikStringSupport::displayString);
-		RandomGenerator<Set<Object>> gen = values.generator(1000, true);
-		for (int i = 0; i < size; i++) {
-			long seed = random.nextLong();
-			assertThat(JqwikStringSupport.displayString(gen.next(SourceOfRandomness.newRandom(seed)).value()))
-				.isEqualTo(JqwikStringSupport.displayString(gen.next(SourceOfRandomness.newRandom(seed)).value()));
-		}
+	@Provide
+	static Arbitrary<ArbitraryTransformer<?, ?>> arbitraryTransformations() {
+		return Arbitraries.of(
+			ArbitraryTransformer.IDENTITY,
+			ArbitraryTransformer.WRAP_WITH_OPAQUE_OBJECT,
+			ArbitraryTransformer.SET,
+			ArbitraryTransformer.SET__MAP_EACH,
+			ArbitraryTransformer.SET__FLAT_MAP_EACH,
+			ArbitraryTransformer.WRAP_WITH_OPAQUE_OBJECT.then(ArbitraryTransformer.SET),
+			ArbitraryTransformer.WRAP_WITH_OPAQUE_OBJECT.then(ArbitraryTransformer.SET__MAP_EACH),
+			ArbitraryTransformer.WRAP_WITH_OPAQUE_OBJECT.then(ArbitraryTransformer.SET__FLAT_MAP_EACH)
+		);
 	}
 }
